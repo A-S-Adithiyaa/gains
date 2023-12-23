19e87d1a5c936e0ccfc5a402a61fc16969b06899
A S Adithiyaa A S <adithiyaa1112@gmail.com>
2023-12-23 12:10:36 +0530

    Updates

---
+++ precache-manifest.8460306c98f61fb8593e422bc3dcdfdc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc6c96bb8ac0079158224c6b6adc634c",
+    "url": "/gains/index.html"
+  },
+  {
+    "revision": "46767a08b3b3c75a2641",
+    "url": "/gains/static/css/main.fe8c169d.chunk.css"
+  },
+  {
+    "revision": "a2bb5363e42a7dd46d79",
+    "url": "/gains/static/js/2.8f260cb2.chunk.js"
+  },
+  {
+    "revision": "46767a08b3b3c75a2641",
+    "url": "/gains/static/js/main.c48c59da.chunk.js"
+  },
+  {
+    "revision": "8c573da911540e0a31ed",
+    "url": "/gains/static/js/runtime~main.50004952.js"
+  }
+]);
