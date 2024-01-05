8b70a4358d0bca93bb88b85f19a3cd681ac5b2d0
A S Adithiyaa A S <adithiyaa1112@gmail.com>
2024-01-05 15:53:14 +0530

    Changed the routing

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
       <NavbarSection />
       <Routes>
         <Route path="/" element={<Landing />} />
-        <Route path="/assessments/" element={<AssessmentLanding />} />
+        <Route path="/assessments" element={<AssessmentLanding />} />
       </Routes>
     </HashRouter>
   );
