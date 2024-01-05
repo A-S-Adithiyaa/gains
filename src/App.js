12d9f99a714c8f12ac3a6650486a66fab032ceb1
A S Adithiyaa A S <adithiyaa1112@gmail.com>
2024-01-05 18:29:16 +0530

    Changed the routing

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import "./App.css";
 import React from "react";
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Landing from "./Landing";
 import AssessmentLanding from "./components/assessments/AssessmentLanding";
 import NavbarSection from "./components/Navbar";
 
 function App() {
   return (
-    <HashRouter>
+    <BrowserRouter basename="/gains">
       <NavbarSection />
       <Routes>
-        <Route path="/" element={<Landing />} />
+        <Route exact path="/gains" element={<Landing />} />
         <Route path="/assessments" element={<AssessmentLanding />} />
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
 
